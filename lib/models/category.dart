2984fb811e69d3afe5403b0b5ccf9df15919a65d
tbansod2018 <tbansod2018@gmail.com>
2024-07-27 23:14:17 +0530

    first commit

---
+++ lib/models/category.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  const Category(this.title, this.color);
+  final String title;
+  final Color color;
+}
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other
+}
